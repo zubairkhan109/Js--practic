a67ace15c5cc58d0d17bba90f104ab1069d8aaf4
zubairkhan <153979667+zubairkhan109@users.noreply.github.com>
2026-06-18 02:28:30 +0500

    Variable String & Number

---
+++ Variable String & Number/app.js
@@ -0,0 +1,40 @@
+let userName = "Zubair";
+let email = "zubair@gmail.com"
+
+alert(userName)
+alert(email)
+
+// Real-Life Examples:
+// User Name
+// Email Address
+// Product Name
+// City Name
+// Chat Messages
+// Error Messages
+
+
+let age = 20;
+let mark = 97;
+let price = 5000;   
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 + num2);
+
+
+let name = "Zubair";
+let age = 20;
+
+console.log("Name", name);
+console.log("Age", age);
+
+
+
+// Real-Life Examples:
+// Age
+// Marks
+// Salary
+// Product Price
+// Quantity
+// Bank Balance
