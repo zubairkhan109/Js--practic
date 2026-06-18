99752e16e430ca84294527cb7224ff781b7a0a4e
zubairkhan <153979667+zubairkhan109@users.noreply.github.com>
2026-06-19 01:12:52 +0500

    Variable legal & illegal

---
+++ Variable legal & illegal/app.js
@@ -0,0 +1,26 @@
+let userName = "Zubair";
+
+// Yahan:
+
+// let → variable banane ka keyword
+// userName → variable ka naam
+// "Zubair" → value
+
+// Legal Variable Names ✅
+// Ye JavaScript mein valid hain:
+
+let userName = "Zubair"
+let age = 20;
+let _name = "Ali";
+let $price = 5000;
+let user123 = "Saif";
+
+
+// Legal Variable Names ✅
+// Ye JavaScript mein valid hain:
+
+let 123user = "Eijaz";
+let user name ="Naveed";
+let user-name ="Rafay";
+
+ 
